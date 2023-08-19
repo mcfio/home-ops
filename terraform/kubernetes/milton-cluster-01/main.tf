227d005a500b90eaac0dd29cece0513c99680337
Nick M <4718+rkage@users.noreply.github.com>
2023-08-19 12:16:50 -0400

    remove .envrc, env vars only for provider auths

--- terraform/kubernetes/milton-cluster-01/main.tf
+++ terraform/kubernetes/milton-cluster-01/main.tf
@@ -38,12 +38,7 @@ terraform {
   }
 }
 
-provider "vsphere" {
-  user                 = var.vsphere_user
-  password             = var.vsphere_password
-  vsphere_server       = var.vsphere_server
-  allow_unverified_ssl = true
-}
+provider "vsphere" {}
 
 provider "talos" {}
 
