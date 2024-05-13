4188faa9847ddc113f930ead03fd9bb9b169fbbb
Nick M <4718+rkage@users.noreply.github.com>
2024-05-13 17:47:02 -0400

    chore: remove old terraform files, no longer used

--- terraform/kubernetes/milton-cluster-01/modules/git-repository/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "name" {
-  value = github_repository.gitops_repo.name
-}
-
-output "full_name" {
-  value = github_repository.gitops_repo.full_name
-}
--- terraform/kubernetes/milton-cluster-01/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "talosconfig" {
-  value = module.talos_cluster.talosconfig
-  sensitive = true
-}
-
-output "kubeconfig" {
-  value = module.talos_cluster.kubeconfig
-  sensitive = true
-}
